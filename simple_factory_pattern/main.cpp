20d9d519065e42081af80bdfd614f7299db1dbdc
pansen <pansen626@sina.com>
2019-03-20 14:36:32 +0800

    add file

--- simple_factory_pattern/main.cpp
+++ simple_factory_pattern/main.cpp
@@ -1,54 +1,41 @@
 #include <iostream>
 #include <string>
 
-class product
-{
+class product {
 public:
     virtual ~product() {}
 };
 
-class phone : public product
-{
+class phone : public product {
 public:
-    phone()
-    {
+    phone() {
         std::cout << "make phone" << std::endl;
     }
 };
 
-class tv : public product
-{
+class tv : public product {
 public:
-    tv()
-    {
+    tv() {
         std::cout << "make tv" << std::endl;
     }
 };
 
-class simple_factory
-{
+class simple_factory {
 public:
-    static product* create(const std::string& product_name)
-    {
-        if (product_name == "phone")
-        {
+    static product *create(const std::string &product_name) {
+        if (product_name == "phone") {
             return new phone();
-        }
-        else if (product_name == "tv")
-        {
+        } else if (product_name == "tv") {
             return new tv();
-        }
-        else
-        {
+        } else {
             return nullptr;
         }
     }
 };
 
-int main()
-{
-    product* product1 = simple_factory::create("phone");
-    product* product2 = simple_factory::create("tv");
+int main() {
+    product *product1 = simple_factory::create("phone");
+    product *product2 = simple_factory::create("tv");
     delete product1;
     delete product2;
     return 0;
